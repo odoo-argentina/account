f817a224c7dcfc9b050653b14193a0b1e5979e16
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-02-12 12:52:25 -0300

    IMp first steps migrating chart

--- l10n_ar_chart/__openerp__.py
+++ l10n_ar_chart/__openerp__.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Argentina - Planes Contables',
-    'author': 'Moldeo Interactive,ADHOC SA,OpenERP - Team de Localización Argentina',
+    'author': 'Moldeo Interactive,ADHOC SA',
     'category': 'Localization/Account Charts',
     'license': 'AGPL-3',
     'depends': [
         'l10n_multilang',
     ],
-    'demo': [
-    ],
     'test': [],
     'data': [
+        'data/account_chart_template.xml',
         'data/account_chart_respinsc.xml',
+        'data/account_tax_template.xml',
+        'data/account_fiscal_template.xml',
+        'account_chart_template.yml',
+        'data/menuitem.xml',
         # TODO include
         # 'data/account_chart_monotrib.xml',
         # 'data/account_chart_coop.xml',
+        # 'data/account_chart_agro.xml',
+    ],
+    'demo': [
+        'demo/l10n_be_demo.yml',
+        '../account/demo/account_bank_statement.yml',
+        '../account/demo/account_invoice_demo.yml',
     ],
     'installable': False,
     'version': '9.0.0.0.0',
